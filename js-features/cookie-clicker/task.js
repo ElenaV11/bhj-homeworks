6d2562a95f9f69fdb5686a0a218a5676ba01bbf3
E Vi <metelevae@mail.ru>
2023-05-22 22:08:10 +0300

    cookie

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,2 +1,9 @@
 const cookie = document.getElementById("cookie");
 const clickCounter = document.getElementById("clicker__counter");
+let clickNumber = 0;
+cookie.onclick = function () {
+    clickNumber++;
+    cookie.width === 200 ? (cookie.width = 150, cookie.height = 150) : (cookie.width = 200, cookie.height = 200);
+    clickCounter.textContent = clickNumber
+}
+
