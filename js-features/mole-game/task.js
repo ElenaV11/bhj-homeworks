71daae6b65b6f3a4352a75dc6e70b8ec19bac015
E Vi <metelevae@mail.ru>
2023-05-25 22:24:53 +0300

    mole game.

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,4 +1,13 @@
-const dead = document.getElementById('dead');
-const lost = document.getElementById('lost');
+const dead = document.getElementById("dead");
+const lost = document.getElementById("lost");
 let deadCount = 0;
 let lostCount = 0;
+const holes = document.querySelectorAll(".hole");
+const Arr = [];
+holes.forEach((node) => {
+    Arr.push(node);
+  });
+  
+  for (let i = 1; i <= Arr.length; i++) {
+   if (Arr(i).classList.contains("hole_has-mole"))
+  }
