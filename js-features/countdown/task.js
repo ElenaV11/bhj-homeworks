98312edcafa2d7e8476b925f54d25e7934d973cc
E Vi <metelevae@mail.ru>
2023-05-25 20:52:06 +0300

    countDown/

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,9 +1,9 @@
 const timerElement = document.getElementById("timer");
 const statusElement = document.getElementById("status");
-
+let timer = timer.textContent;
 let countDown = setInterval(function() {
 	timer--;
-	timerElement.innerText(timer);
+	timerElement.textContent = timer;
 
 	if (timer <= 0) {
 		clearInterval(countDown);
