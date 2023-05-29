c4698ad4f6e5847793742190ca8baf9b6531f395
E Vi <metelevae@mail.ru>
2023-05-29 17:22:12 +0300

    tabs

---
+++ event-object/tabs/task.js
@@ -0,0 +1,9 @@
+const tabs = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab__content");
+
+tabs.forEach(   // создаем массив и что-то с ним нужно сделать...
+
+    tab.classList.add('tab_active');
+    tabContents[index].classList.add('tab__content_active'); //выбранная вкладка, верное содержимое
+
+)
