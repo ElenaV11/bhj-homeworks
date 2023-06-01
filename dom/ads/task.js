1ecc4b9dd12a49f8b4f3bed57b1c27e1850a473e
E Vi <metelevae@mail.ru>
2023-06-01 23:03:13 +0300

    rotate

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,16 +1,12 @@
 const cases = document.querySelectorAll(".rotator__case");
-const Arr = [];
-cases.forEach((node) => {
-    Arr.push(node);
-  });
 
 function rotate() {
-	for (let i = 0; i < Arr.length; i++) {
-		if (Arr[i].classList.contains("rotator__case_active")) {
-			Arr[i].classList.remove("rotator__case_active");
+	for (let i = 0; i < cases.length; i++) {
+		if (cases[i].classList.contains("rotator__case_active")) {
+			cases[i].classList.remove("rotator__case_active");
     
-            const nextEl = (i + 1) % Arr.length;
-            Arr[nextEl].classList.add("rotator__case_active"); 
+            const nextEl = (i + 1) % cases.length;
+            cases[nextEl].classList.add("rotator__case_active"); 
 		
 		}
 	}
