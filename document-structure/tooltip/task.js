65bb6ab7ff5d742dc6b6a706845d413b875287de
E Vi <metelevae@mail.ru>
2024-02-24 17:25:27 +0300

    tooltip1

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -4,19 +4,19 @@ tooltip.classList.add('tooltip');
 console.log(tooltip);
 
 
-hasTooltip.forEach(e => {
-    e.addEventListener('click', (e) => {
+hasTooltip.forEach(element => {
+    element.addEventListener('click', (e) => {
         e.preventDefault();
-        if (e.contains(document.querySelector('.tooltip_active'))) {
+        if (element.contains(document.querySelector('.tooltip_active'))) {
             tooltip.classList.remove('tooltip_active');
         } else {
             tooltip.classList.add('tooltip_active');
-            tooltip.innerText = e.title;
-            e.appendChild(tooltip);
+            tooltip.innerText = element.title;
+            element.appendChild(tooltip);
 
             let { bottom, left } = element.getBoundingClientRect();
-            tooltip.style.top = bottom + 'px';
-            tooltip.style.left = left + 'px';
+            tooltip.style.top = bottom + "px";
+            tooltip.style.left = left + "px";
         }
     });
 });
