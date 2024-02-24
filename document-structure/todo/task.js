ad7ec3ac287812b62d8cb1105b95ef46c6fe339c
E Vi <metelevae@mail.ru>
2024-02-24 17:03:10 +0300

    todo1

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,23 +1,37 @@
-const form = document.getElementById('tasks__form');
-const input = document.getElementById('task__input');
+const tasksForm = document.getElementById('tasks__form');
+const taskInput = document.getElementById('task__input');
 const tasksList = document.getElementById('tasks__list');
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    
-    if (input.value.trim()) {
-        tasksList.insertAdjacentHTML('afterBegin', 
-            `<div class="task">
-                <div class="task__title">
-                    ${input.value.trim()}
-                </div>
-                <a href="#" class="task__remove">&times;</a>
-            </div>`);
-            
-        const taskClose = document.querySelector('.task__remove');
-        taskClose.addEventListener('click', (e) => {
-            e.target.closest('.task').remove();
-        });
-    } 
-    form.reset();
-});
+function createTask(title) {
+	const taskElement = document.createElement('div');
+	taskElement.classList.add('task');
+
+	const titleElement = document.createElement('div');
+	titleElement.classList.add('task__title');
+	titleElement.textContent = title;
+
+	const removeBtn = document.createElement('a');
+	removeBtn.classList.add('task__remove');
+	removeBtn.innerHTML = '&times;';
+	removeBtn.addEventListener('click', () => {
+		taskElement.remove(); 
+	});
+
+	taskElement.appendChild(titleElement);
+	taskElement.appendChild(removeBtn);
+
+	return taskElement;
+}
+
+function addTask(e) {
+	e.preventDefault(); 
+
+	const title = taskInput.value.trim();
+	if (title !== '') {
+		const task = createTask(title);
+		tasksList.appendChild(task);
+		taskInput.value = '';
+	}
+}
+
+tasksForm.addEventListener('submit', addTask);
