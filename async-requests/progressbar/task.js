d8e848640fc6e1e89022cb9b2cc247116cc30b4f
E Vi <metelevae@mail.ru>
2024-02-28 20:36:50 +0300

    progressbar1

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,5 +1,5 @@
 const progress = document.getElementById('progress');
-const form = document.getElementByIdr('form');
+const form = document.getElementById('form');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
